1d5fcea6941fa87ee892feb9bf337d1e9e3d0204
Manjari <lk659879@dal.ca>
2020-03-18 14:11:43 -0300

    variable name mismatch

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,36 +1,37 @@
 module.exports = (app, con) => {
-  app.post("/users", (req, res) => {
-    const {
-      body: { firstname, lastname, email, password, age, gender } = {}
-    } = req;
+    app.post("/users", (req, res) => {
 
-    if (firstname && lastname && email && gender) {
-      console.log("Register Request received");
-      con.connect(err => {
-        con.query(
-          `INSERT INTO ccgroup7.users (firstname, lastname, email, password, age, gender)
+        const {
+            body: { firstName, lastName, email, password, age, gender } = {}
+        } = req;
+
+        if (firstName && lastName && email && gender) {
+            console.log("Register Request received");
+            con.connect(err => {
+                con.query(
+                    `INSERT INTO ccgroup7.users (firstname, lastname, email, password, age, gender)
             VALUES ('${firstname}', '${lastname}', '${email}', '${password}', '${age}', '${gender}')`,
-          (err, result, fields) => {
-            if (err) res.send(err);
-            if (result) res.send(result);
-          }
-        );
-      });
-    } else {
-      console.log("Missing a parameter");
-    }
-  });
-
-  // GET api to get users
-  app.get("/users", (req, res) => {
-    con.connect(err => {
-      con.query(
-        `select firstname, lastname, email, age from ccgroup7.users;`,
-        (err, result, fields) => {
-          if (err) res.send(err);
-          if (result) res.send(result);
+                    (err, result, fields) => {
+                        if (err) res.send(err);
+                        if (result) res.send(result);
+                    }
+                );
+            });
+        } else {
+            console.log("Missing a parameter");
         }
-      );
     });
-  });
+
+    // GET api to get users
+    app.get("/users", (req, res) => {
+        con.connect(err => {
+            con.query(
+                `select firstname, lastname, email, age from ccgroup7.users;`,
+                (err, result, fields) => {
+                    if (err) res.send(err);
+                    if (result) res.send(result);
+                }
+            );
+        });
+    });
 };
